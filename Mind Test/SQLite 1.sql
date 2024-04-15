67753dda78a02dfe6beda9bc89cecb4bb667cb77
Novitasari12w <165393399+Novitasari12w@users.noreply.github.com>
2024-04-15 21:14:00 +0700

    Add files via upload

---
+++ Mind Test/SQLite 1.sql
@@ -0,0 +1,8 @@
+SELECT * FROM demo;
+TABLE = { 'employee' : ['Alice', 'Benn', 'Charles', 'Dorothy', 'Emma', 'Franklin'],
+			'salary' : [11, 43, 33, 55, 22,33]
+            } 
+
+urutan_data = DESC(SET(TABLE['salary'])
+                   SELECT FROM TABLE (urutan_data)
+                      
